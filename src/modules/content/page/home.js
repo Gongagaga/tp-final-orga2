c3aac668b6982459aeb3235d9ba802540de96a8f
Gongagaga <gwendal.nog@gmail.com>
2023-09-07 15:56:14 +0200

    lighthouse commit

--- src/modules/content/page/home.js
+++ src/modules/content/page/home.js
@@ -6,10 +6,10 @@ export const homeText = () => {
   homeDiv.setAttribute("x-transition:leave", "fade-leave");
 
   let homeText = `   
-        <h3>Bienvenue sur "Convertis mon kiwi" !</h3>
-        <h3>Notre site simplifie vos conversions d'unités, que ce soit des kilogrammes, des décimales ou des températures.</h3>
-        <h3>Ne cherchez plus, notre outil convivial est là pour rendre vos conversions aussi simples que de croquer dans un kiwi bien mûr.</h3>
-        <h3>Rejoignez-nous pour simplifier votre quotidien en un seul clic.</h3>
+        <h2>Bienvenue sur "Convertis mon kiwi" !</h2>
+        <h2>Notre site simplifie vos conversions d'unités, que ce soit des kilogrammes, des décimales ou des températures.</h2>
+        <h2>Ne cherchez plus, notre outil convivial est là pour rendre vos conversions aussi simples que de croquer dans un kiwi bien mûr.</h2>
+        <h2>Rejoignez-nous pour simplifier votre quotidien en un seul clic.</h2>
     `;
 
   homeDiv.innerHTML = homeText;
--- src/modules/footer/footer.js
+++ src/modules/footer/footer.js
@@ -4,7 +4,7 @@ export const footer = () => {
   footer.innerHTML = `
     <h3>Copyright reserved : KiwiConvertor.Inc 2023</h3>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint dolorem laborum quas quaerat error! Dolorum minus sed consequatur quidem expedita dignissimos beatae nostrum ratione! Illo, nemo corrupti. Aliquam, modi. Pariatur!</p>
-    <h5>Contact Us !</h5>
+    <h4>Contact Us !</h4>
     <p>Mail : <a href="mailto:KiwiConvertor@kiwi.fr"> KiwiConvertor@kiwi.fr </a></p>
     <p>Phone : <a href="tel:+33 1 23 45 67 89"> +33 1 23 45 67 89 </a></p>
   `;
